9bd4d6fd7e5470017dad06bcb43c96e2bc9c3bb4
cloudynn <114894314+cloudynn@users.noreply.github.com>
2022-10-03 19:09:45 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,90 @@
+let __layar = parseInt(document.getElementById("layar").value);
+
+function __1() {
+    let layar;
+
+    layar = document.getElementById("1").value;
+
+    document.getElementById("layar").value = layar;
+
+}
+
+function __2() {
+    let layar;
+
+    layar = document.getElementById("2").value;
+
+    document.getElementById("layar").value = layar;
+}
+
+function __3() {
+    let layar;
+
+    layar = document.getElementById("3").value;
+
+    document.getElementById("layar").value = layar;
+}
+
+function __4() {
+    let layar;
+
+    layar = document.getElementById("4").value;
+
+    document.getElementById("layar").value = layar;
+}
+
+function __5() {
+    let layar;
+
+    layar = document.getElementById("5").value;
+
+    document.getElementById("layar").value = layar;
+}
+
+function __6() {
+    let layar;
+
+    layar = document.getElementById("6").value;
+
+    document.getElementById("layar").value = layar;
+}
+
+function __7() {
+    let layar;
+
+    layar = document.getElementById("7").value;
+
+    document.getElementById("layar").value = layar;
+}
+
+function __8() {
+    let layar;
+
+    layar = document.getElementById("8").value;
+
+    document.getElementById("layar").value = layar;
+}
+
+function __9() {
+    let layar;
+
+    layar = document.getElementById("9").value;
+
+    document.getElementById("layar").value = layar;
+}
+
+function __0() {
+    let layar;
+
+    layar = document.getElementById("0").value;
+
+    document.getElementById("layar").value = layar;
+}
+
+function __d() {
+    let layar;
+
+    layar = document.getElementById("D").value;
+
+    document.getElementById("layar").value = layar;
+}
